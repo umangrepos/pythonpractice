4fde24ae1156a23b9f1f737ed7a865e748e607d1
test <test>
2024-08-22 11:05:40 +0530

    new commit

---
+++ dictionaries/mergetwodictionaries.py
@@ -0,0 +1,8 @@
+my_dict1 = {"a":1,"b":2,"c":3}
+my_dict2 = {"c":1,"d":2,"e":3}
+
+
+final_dict = my_dict1 | my_dict2
+
+
+print(final_dict)
---
+++ listsandtuples/printelementoflistinsingleline.py
@@ -0,0 +1,8 @@
+L = [3,4,5,6]
+
+
+#for i in L :
+#    print(i,end=" ")
+print(*L,sep=" ")
+
+
