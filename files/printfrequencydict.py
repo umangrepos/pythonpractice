d3e63dd8d7b02995d196447a42e3fe87458aa383
test <test>
2024-09-26 15:48:11 +0530

    new commit

---
+++ files/printfrequencydict.py
@@ -0,0 +1,15 @@
+file_path = "/home/umang/Downloads/pythonpractice/files/basicfile2.txt"
+
+freq_dict={}
+
+with open(file_path) as file:
+    for word in file:
+        word = word.strip("\n")
+        if word not in freq_dict :
+            freq_dict[word] = 1
+        else :
+            freq_dict[word] += 1
+
+
+
+print(freq_dict)
