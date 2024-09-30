9fedb26ef3731fe1b57825f3c911309dfe6ae563
test <test>
2024-09-30 09:55:51 +0530

    new commit

---
+++ listsandtuples/secondlargestvalueinlist.py
@@ -0,0 +1,16 @@
+ListA = [5,4,3,2]
+
+
+if len(ListA)>1:
+   sorted_list = sorted(ListA)
+   print(sorted_list[-2])
+else:
+   print(None)
+
+# if len(ListA)>1:
+#     no_duplicates = set(ListA)
+#     no_duplicates.remove(max(no_duplicates))
+#     print(max(no_duplicates))
+# else:
+#     print(None)
+ 
