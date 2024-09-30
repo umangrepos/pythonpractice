b1f80b46fa1b30ee94c9e083a5e0dc3d441690a2
test <test>
2024-09-30 15:00:49 +0530

    new commit

--- loops/checknumberisprime.py
+++ loops/checknumberisprime.py
@@ -1,20 +1,32 @@
-is_prime = True
+#is_prime = True
 
 n = int(input("enter an integer: "))
 
-if n == 1:
-    is_prime = False
+#if n==0 or n == 1:
+#    is_prime = False
+#else:
+#    for i in range(2,n):
+#         if n % i == 0 :
+#             is_prime = False
+#             break
+# if is_prime:
+#     print("prime")
+# else:
+#     print("not prime")
+
+
+
+
+
+if n==0 or n == 1:
+   print("not prime")
 else:
     for i in range(2,n):
-        if i % 2 == 0 :
-            is_prime = False
+        if n % i == 0 :
+            print("not prime")
             break
-
-if is_prime:
-    print("prime")
-else:
-    print("not prime")
-
-
+        
+    else:
+        print("prime")  
 
 
