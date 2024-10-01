33e5821b1a89714c5b91fd1215434c7c281bc9b8
test <test>
2024-10-01 16:55:37 +0530

    new commit

--- conditionals/leapyear.py
+++ conditionals/leapyear.py
@@ -1,12 +1,37 @@
-a = 1
-b = 3
-c = 5
+year = 1836
 
-if (a >= b) and (a >= c):
-    print(a)
-elif (b >= a) and (b >= c):
-    print(b)
+# if year % 4 == 0 :
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print(f"{year} is a leap year")
+#         else:
+#             print(f"{year} is a common year")
+#     else:
+#         print(f"{year} is a leap year")
+
+# else:
+#     print(f"{year} is a common year")
+
+
+# option 2
+# if year % 400 == 0:
+#     print(f"{year} is a leap year")
+# elif year % 100 == 0:
+#     print(f"{year} is not a leap year")
+# elif year % 400 == 0:
+#     print(f"{year} is a leap year")
+# else:
+#     print(f"{year} is not a leap year")
+
+
+#option 3
+if year % 4 != 0:
+    print(f"{year} is not a leap year")
+elif year % 100 != 0:
+    print(f"{year} is a leap year")
+elif year % 400 != 0:
+    print(f"{year} is not a leap year")
 else:
-    print(c)
+    print(f"{year} is a leap year")
 
-#print(max(a,b,c))
+
