8f48973d874e8c19bd683134d967181883e4a183
test <test>
2024-10-05 18:21:52 +0530

    new commit

---
+++ loops/floidstriangle.py
@@ -0,0 +1,12 @@
+n = 3
+
+count = 1
+
+for i in range (0,n+1):
+    for j in range(0,i):
+        print(count,end=" ")
+        count += 1
+    print()
+
+    
+
--- loops/printhalfpyramidusingloops.py
+++ loops/printhalfpyramidusingloops.py
@@ -1,9 +1,14 @@
 num_rows = int(input("enter number of rows:"))
+k = (2*num_rows)-2
 
 for i in range(0,num_rows) :
-    for j in range(0,num_rows):
-        print(" * ",end="")
-    print()
+    for j in range(0,k):
+        print("",end=" ")
+    k = k-2
+    for j in range(0,i+1):
+        print("*",end=" ")
     
+    print("")
 
+    
 
