3fcff8b87331dd443affbbe2e32c1a63b5133db2
test <test>
2024-10-07 12:11:44 +0530

    new commit

---
+++ recursion/decimaltobinary.py
@@ -0,0 +1,7 @@
+def convert_to_binary(decimalnum):
+    if decimalnum == 0:
+        return '0'
+    else:
+        return (convert_to_binary(decimalnum//2)+ str(decimalnum%2)).lstrip("0") 
+    
+print(convert_to_binary(5))
