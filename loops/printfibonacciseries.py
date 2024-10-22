3a517bb7b277c5463e0c804f8c1037c60b8912e9
Your Name <you@example.com>
2024-10-22 15:18:57 +0530

    new commit

--- loops/printfibonacciseries.py
+++ loops/printfibonacciseries.py
@@ -1,9 +1,8 @@
-n = int(input("enter the number"))
-
-factorial = 1
-for i in range (2,n+1) :
-    factorial *= 1
-
-print(factorial)
-
-
+a=0
+b=1
+n=10
+for i in range(2,n):
+    c=a+b
+    a=b
+    b=c
+    print(c)
