fb1388a1aeeab31fb1e2a29ac4da216fff756bce
Your Name <you@example.com>
2024-10-22 15:24:18 +0530

    new commit

--- loops/factorial.py
+++ loops/factorial.py
@@ -1,12 +1,7 @@
-n = 3
-
-count = 1
-
-for i in range (0,n+1):
-    for j in range(0,i):
-        print(count,end=" ")
-        count += 1
-    print()
-
+n = int(input("enter the number"))
+factorial = 1
+for i in range (2,n+1) :
+    factorial *= 1
+print(factorial)
     
 
